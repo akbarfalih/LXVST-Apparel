002055b52be767546034e6a7b2354313d7a3c983
Akbar Falih <akbarfalih99@gmail.com>
2023-03-13 22:18:52 +0700

    membuat website  marketplace apparel

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// Toggle class active
+const navbarCenter = document.querySelector('.navbar-center');
+// Klik Hamburger toggle sidebar
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarCenter.classList.toggle('active');
+};
+
+// Menghilangkan sidebar
+const hamburger = document.querySelector('#hamburger-menu');
+
+
+document.addEventListener('click', function(e){
+    if(!hamburger.contains(e.target) && !navbarCenter.contains(e.target)) {
+        navbarCenter.classList.remove('active');
+    }
+});
+
+// Swiper JS
+var swiper = new Swiper('.slide-container', {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    slidesPerGroup: 4,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+
